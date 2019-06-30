4660bafdc538a68b0d5c0427dd888a7650521db6
Srini Seetharaman <seethara@stanford.edu>
2019-06-30 10:54:43 -0700

    Ready for test in ONOS tutorial

--- Projeto/ringMininet.py
+++ Projeto/ringMininet.py
@@ -15,7 +15,7 @@ from mininet.cli import CLI
 from functools import partial
 from mininet.node import RemoteController
 from mininet.node import OVSController
-from mininet.node import OVSKernelSwitch
+from mininet.node import OVSKernelSwitch, Host
 import os
 from time import sleep
 
@@ -30,14 +30,14 @@ class MyTopo(Topo):
         s3 = self.addSwitch('s3', cls=OVSKernelSwitch)
 
         #Client-Host
-        client1 = self.addHost('client1', ip='10.0.0.1')
-        client2 = self.addHost('client2', ip='10.0.0.3')
-        client3 = self.addHost('client3', ip='10.0.0.5')
+        client1 = self.addHost('client1', cls=Host, defaultRoute=None, ip='10.0.0.1/24')
+        client2 = self.addHost('client2', cls=Host, defaultRoute=None, ip='10.0.0.3/24')
+        client3 = self.addHost('client3', cls=Host, defaultRoute=None, ip='10.0.0.5/24')
 
         #Server-Host
-        server1 = self.addHost('server1',ip='10.0.0.2')
-        server2 = self.addHost('server2',ip='10.0.0.4')
-        server3 = self.addHost('server3',ip='10.0.0.6')
+        server1 = self.addHost('server1', cls=Host, defaultRoute=None, ip='10.0.0.2/24')
+        server2 = self.addHost('server2', cls=Host, defaultRoute=None, ip='10.0.0.4/24')
+        server3 = self.addHost('server3', cls=Host, defaultRoute=None, ip='10.0.0.6/24')
 
         #Links between switches
         self.addLink(s1, s2, bw=0.24, delay='0ms', loss=0, max_queue_size=1000)
@@ -67,16 +67,20 @@ def configureNetwork():
     #Tenho que apontar o IP para a instancia do ONOS.
 
     net = Mininet(topo=topo, build=False, link=TCLink)
+
     controlador = net.addController(name='controlador',
     controller=RemoteController,
-    ip='127.0.0.1',
+    ip='127.17.0.1',
     protocol='tcp',
     port=6633)
     
     net.start()
+    # net.build()
+
     print ("Dumping host connections")
     dumpNodeConnections(net.hosts)
     
+
     client1 = net.get('client1')
     client2 = net.get('client2')
     client3 = net.get('client3')
@@ -89,7 +93,7 @@ def configureNetwork():
     client3.setMAC("0:0:0:0:0:5")
     server1.setMAC("0:0:0:0:0:2")
     server2.setMAC("0:0:0:0:0:4")
-    server3.setMAC("0:0:0:0:0:6")
+    server3.setMAC("0:0:0:0:0:6")   
 
     CLI(net)
     net.stop()
